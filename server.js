303ff34bdf00cfb3967ce98367d17e35cd5493f0
shiqing881215 <shiqing881215@gmail.com>
2015-07-01 00:04:53 -0700

    Add multiple time frame

--- server.js
+++ server.js
@@ -4,39 +4,52 @@ var moment = require('moment');
 
 var app = express();
 
-var SYMBOLS = ['CRM','FB','APPL','GOOG','YHOO','EA','SCTY','GRPN','BABA','MSFT','AMZN','JD','LNKD','BIDU','TWTR','HDP'];
-var allDone = 0;
-var allQuotes = [];
+var SYMBOLS = ['CRM','SCTY','APPL','AMZN','BABA','EA','FB','GOOG','GRPN','HDP','LNKD','MSFT','TWTR','YHOO','BIDU','JD','TECHY','SOHU','SINA','QIHU','NETS'];
+var PERIOD = [7,30,90,180,365,730];
 
-SYMBOLS.forEach(getStockHistoricalPrice);
+// Initialize 
+var allDone = {}, allQuotes = {};
+for (i = 0; i < PERIOD.length; i++) {
+	allDone[PERIOD[i].toString()] = 0;
+	allQuotes[PERIOD[i].toString()] = [];
+}
+
+for (i = 0; i < PERIOD.length; i++) {
+	for (j = 0; j < SYMBOLS.length; j++) {
+		// Note this is async call and we don't wait the response back
+		getStockHistoricalPrice(SYMBOLS[j], PERIOD[i]);	
+	} 
+}
 
 app.listen(3000);
 console.log('Server start on 3000');
 
-function getStockHistoricalPrice(symbol) {
-	debugger;
+function getStockHistoricalPrice(symbol, numOfDays) {
 	yahooFinance.historical({
 		symbol: symbol,
-		from: moment().subtract(90,'days').format().slice(0,10),
+		from: moment().subtract(numOfDays,'days').format().slice(0,10),
 		to: moment().format().slice(0,10)
 		// period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
 	}, function (err, quotes) {
 		// console.log(quotes);
-		allDone++;
+		allDone[numOfDays.toString()]++;
+		// console.log(allDone + " : " + symbol + " : " + numOfDays);
 		if (quotes) {
-			allQuotes.push(quotes);
+			allQuotes[numOfDays.toString()].push(quotes);
 		}
 
 		// After get all symbol price then set up the Express
-		if (allDone == SYMBOLS.length) {
-			app.get('/yahooFinance', function(req, res){
+		if (allDone[numOfDays] == SYMBOLS.length) {
+			app.get('/yahooFinance/' + numOfDays, function(req, res){
 				// Define the required header. Important
 				res.setHeader('Content-Type', 'application/json');
 				res.setHeader('Access-Control-Allow-Origin', '*');
 			    
 			    // Send back the data
-			    res.json(allQuotes);
+			    res.json(allQuotes[numOfDays.toString()]);
 			});
+			console.log('Endpoint : /yahooFinance/' + numOfDays + ' is ready to use');
+
 		}
 	});
 }
